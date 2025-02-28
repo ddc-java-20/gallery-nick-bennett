525a2c8f4ebd5b22c0d104bd8a9f8b30917f22a9
nick-bennett <nbennett10@cnm.edu>
2025-02-28 10:44:46 -0700

    Working with the calendar widget.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/CalendarFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/CalendarFragment.java
@@ -8,20 +8,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import com.kizitonwose.calendar.core.CalendarDay;
+import com.kizitonwose.calendar.view.MonthDayBinder;
 import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.nasaapod.adapter.DayHolder;
+import edu.cnm.deepdive.nasaapod.databinding.FragmentCalendarBinding;
 import edu.cnm.deepdive.nasaapod.viewmodel.ApodViewModel;
+import org.jetbrains.annotations.NotNull;
 
 @AndroidEntryPoint
 public class CalendarFragment extends Fragment {
 
+  private FragmentCalendarBinding binding;
   private ApodViewModel viewModel;
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater,
       @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    binding = FragmentCalendarBinding.inflate(inflater, container, false);
+    binding.calendar.setDayBinder(new MonthDayBinder<DayHolder>() {
+
+      @Override
+      public void bind(@NotNull DayHolder holder, CalendarDay calendarDay) {
+        holder.bind(calendarDay);
+      }
+
+      @Override
+      public @NotNull DayHolder create(@NotNull View view) {
+        return new DayHolder(view);
+      }
+
+    });
     // TODO: 2025-02-28 Initialize UI.
-    return super.onCreateView(inflater, container, savedInstanceState);
+    return binding.getRoot();
   }
 
   @Override
@@ -29,7 +49,7 @@ public class CalendarFragment extends Fragment {
     super.onViewCreated(view, savedInstanceState);
     viewModel = new ViewModelProvider(requireActivity())
         .get(ApodViewModel.class);
-    // TODO: 2025-02-28 Observe livedata and start asynchronous processes, as necessary. 
+    // TODO: 2025-02-28 Observe livedata and start asynchronous processes, as necessary.
   }
 
   @Override
