044dc42d0ee609c04b577ed6f032c4a65a7f7b25
nick-bennett <nbennett10@cnm.edu>
2025-03-04 17:38:00 -0700

    Mostly complete (for now).

--- app/src/main/java/edu/cnm/deepdive/nasaapod/hilt/PicassoModule.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/hilt/PicassoModule.java
@@ -1,23 +1,20 @@
 package edu.cnm.deepdive.nasaapod.hilt;
 
-import android.app.Application;
 import android.content.Context;
 import com.squareup.picasso.Picasso;
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
-import dagger.hilt.android.components.ActivityComponent;
 import dagger.hilt.android.components.FragmentComponent;
 import dagger.hilt.android.qualifiers.ApplicationContext;
-import dagger.hilt.android.scopes.ActivityScoped;
-import javax.inject.Singleton;
+import dagger.hilt.android.scopes.FragmentScoped;
 
 @Module
-@InstallIn(ActivityComponent.class)
+@InstallIn(FragmentComponent.class)
 public class PicassoModule {
 
   @Provides
-  @ActivityScoped
+  @FragmentScoped
   Picasso providePicasso(@ApplicationContext Context context) {
     return new Picasso.Builder(context)
         .build();
